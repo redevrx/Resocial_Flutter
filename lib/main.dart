7c79116c5e5d84c78bcf0c88bb09bbc3eac23f2a
redevrx <redev.rx@gmail.com>
2021-03-10 22:50:32 +0700

    notification fix 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,12 @@ import 'localizations/languages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().whenComplete(() async {
-    print('Firebase Initial Complete');
-    final notifyService = PushNotificationService();
-    await notifyService.initialise();
-    notifyService.getDeviceToken();
-  });
+  await Firebase.initializeApp();
+  //
+  print('Firebase Initial Complete');
+  final notifyService = PushNotificationService();
+  await notifyService.initialise();
+  notifyService.getDeviceToken();
   if (kIsWeb) {
     // Disable persistence on web platforms
     await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
